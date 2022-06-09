b43382e76e2a38b924976e270670b1ae8701c76a
xeusteerapat <xeus085@gmail.com>
2022-06-09 22:54:07 +0700

    :wrench: [Update] change port number

--- server/index.js
+++ server/index.js
@@ -6,7 +6,7 @@ async function main() {
   const IS_GOOGLE_CLOUD_RUN = process.env.K_SERVICE !== undefined;
 
   // You must listen on the port Cloud Run provides
-  const port = process.env.PORT || 3001;
+  const port = process.env.PORT || 8080;
 
   // You must listen on all IPV4 addresses in Cloud Run
   const address = IS_GOOGLE_CLOUD_RUN ? '0.0.0.0' : undefined;
