4bc3d61f7b86e5acfd80b8062d9c88458b596224
xeusteerapat <xeus085@gmail.com>
2022-06-10 09:49:41 +0700

    :bug: [Fix] change port again!!

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,7 +7,7 @@ function App() {
 
   useEffect(() => {
     const fetchUsers = async () => {
-      const res = await fetch('http://localhost:8080/users');
+      const res = await fetch('http://localhost:32437/users');
       const data = await res.json();
 
       setUsers(data);
